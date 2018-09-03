0de23ad75fad6a138b46644245c390ade5533bcc
tarek <tarekmonjur@gmail.com>
2018-09-03 07:55:17 -0700

    push

---
+++ prototype-todo/js/advance_todo.js
@@ -0,0 +1,164 @@
+function Common()
+{
+    console.log('common instance...');
+}
+
+Common.prototype.convertJson = function convertArrayToJson(data){
+    let objData = {};
+    data.forEach(function(value, key){
+        objData[value.name] = value.value;
+    });
+    return objData;
+};
+
+
+
+function Author()
+{
+    Common.call(this);
+    this.authors = [];
+    console.log('author instance...');
+}
+
+Author.prototype = Object.create(Common.prototype);
+Author.prototype.constructor = Author;
+
+Author.prototype.getAuthors = function fn1(){
+    let authors2 = this.authors.slice();
+    return authors2.reverse();
+};
+
+Author.prototype.addAuthor = function fn2(formData){
+    let authorData;
+    authorData = this.convertJson(formData);
+    this.authors.push({
+        'name' : authorData.author_name,
+        'age' : authorData.author_age,
+        'address' : authorData.author_address,
+    });
+    book.renderAuthor();
+};
+
+Author.prototype.renderAuthor = function fun3(){
+    var html = "<select class='form-control' name='author_id'>";
+    this.getAuthors().forEach((obj, key) => {
+        html += "<option value='"+key+"'>"+obj.name+"</option>";
+    });
+    html += "</select>";
+    document.getElementById('authors').innerHTML = html;
+};
+
+
+
+function Book()
+{
+    Author.call(this);
+    this.books = [];
+    this.bookKey;
+    console.log('book instance...');
+    this.renderAuthor();
+}
+
+Book.prototype = Object.create(Author.prototype);
+Book.prototype.constructor = Book;
+
+Book.prototype.getBooks = function get(){
+    return this.books;
+};
+
+Book.prototype.addBook = function add(formData){
+    let bookData = {};
+    bookData = this.convertJson(formData);
+
+    this.books.splice(0,0,{
+        'name' : bookData.book_name,
+        'author' : this.authors.find((obj, key) => { return key == bookData.author_id}),
+        'year' : bookData.publish_year,
+        'description' : bookData.description,
+    });
+    this.renderBook();
+};
+
+Book.prototype.editBook = function edit(){
+    let book = this.books.find((obj, key) => { return key == this.bookKey});
+    document.getElementById('book_name').value = book.name;
+    // document.getElementById('author_name').value = book.author;
+    document.getElementById('publish_year').value = book.year;
+    document.getElementById('description').value = book.description;
+};
+
+Book.prototype.updateBook  = function update(formData){
+    bookData = this.convertJson(formData);
+    let book = {
+        'name' : bookData.book_name,
+        'author' : this.authors.find((obj, key) => { return key == bookData.author_id}),
+        'year' : bookData.publish_year,
+        'description' : bookData.description,
+    };
+    this.books[this.bookKey] = book;
+    this.renderBook();
+};
+
+Book.prototype.deleteBook = function deleteBook(key){
+    this.books.pop(key);
+    this.renderBook();
+};
+
+Book.prototype.renderBook = function renderView(){
+    var html = "";
+    this.getBooks().forEach(function(value, key){
+        html += "<tr><td>"+  value.name + "</td>"+
+            "<td>"+  value.author.name + "</td>"+
+            "<td>"+  value.year + "</td>"+
+            "<td>"+  value.description + "</td>"+
+            "<td> <a class='edit' href='javascript:void(0)' data='"+ key +"'>Edit</a> | <a class='delete' href='javascript:void(0)' data='"+ key +"'>Delete</a></td>" +
+            "</tr>";
+    });
+    $("#books").html(html);
+};
+
+
+
+
+const book = new Book();
+
+$(document).on('submit', '#author_add_form', function(e){
+    e.preventDefault();
+    var formData = $(this).serializeArray();
+    $(this)[0].reset();
+    book.addAuthor(formData);
+    $("#addAuthorModal").modal('hide');
+});
+
+$(document).on('click', '#add_book', function(e){
+    e.preventDefault();
+    let formData = $("#book_form").serializeArray();
+    book.addBook(formData);
+    $("#book_form")[0].reset();
+});
+
+$(document).on('click', '.delete', function(){
+    let dataKey = $(this).attr('data');
+    book.deleteBook(dataKey);
+});
+
+$(document).on('click', '.edit', function(){
+    let dataKey = $(this).attr('data');
+    $(".action_btn").attr('id', 'update');
+    $(".action_btn").html('Update Book To List');
+    $(".form_title").html('Update Book To List');
+    book.bookKey = dataKey;
+    book.editBook();
+});
+
+$(document).on('click', '#update', function(e){
+    e.preventDefault();
+    let formData = $("#formData").serializeArray();
+    book.updateBook(formData);
+    $(".action_btn").attr('id', 'add');
+    $(".action_btn").html('Add Book To List');
+    $(".form_title").html('Add Book To List');
+    $("#formData")[0].reset();
+});
+
+
---
+++ prototype-todo/js/todo.js
@@ -0,0 +1,101 @@
+function Todo(){
+    this.books = [];
+    this.bookKey;
+    console.log('start todo...');
+}
+
+Todo.prototype.gets = function getBooks(){
+    return this.books;
+};
+
+Todo.prototype.add = function addBook(formData){
+    let book = this.convertJson(formData);
+    this.books.push(book);
+    console.log(this.books);
+    this.renderView();
+};
+
+Todo.prototype.convertJson = function convertArrayToJson(data){
+    let objData = {};
+    data.forEach(function(value, key){
+        objData[value.name] = value.value;
+    });
+    return objData;
+};
+
+Todo.prototype.renderView = function renderView(){
+    var html = "";
+    this.gets().forEach(function(value, key){
+        html += "<tr><td>"+  value.book_name + "</td>"+
+        "<td>"+  value.author_name + "</td>"+
+        "<td>"+  value.publish_year + "</td>"+
+        "<td>"+  value.description + "</td>"+
+        "<td> <a class='edit' href='javascript:void(0)' data='"+ key +"'>Edit</a> | <a class='delete' href='javascript:void(0)' data='"+ key +"'>Delete</a></td>" +
+        "</tr>";
+    });
+    $("#books").html(html);
+};
+
+Todo.prototype.delete = function deleteBook(key){
+    this.books.splice(key, 1);
+    this.renderView();
+};
+
+Todo.prototype.edit = function editBook(){
+    let book = this.books.find((obj, key) => { return key == this.bookKey;});
+
+    let book_name = document.getElementById('book_name');
+    let author_name = document.getElementById('author_name');
+    let publish_year = document.getElementById('publish_year');
+    let description = document.getElementById('description');
+
+    book_name.value = book.book_name;
+    author_name.value = book.author_name;
+    publish_year.value = book.publish_year;
+    description.value = book.description;
+};
+
+Todo.prototype.update = function updateBook(formData){
+    let book = this.convertJson(formData);
+    this.books[this.bookKey] = book;
+    console.log(this.bookKey);
+    this.renderView();
+};
+
+
+
+
+const book = new Todo();
+
+$(document).on('click', '#add', function(e){
+    e.preventDefault();
+    let formData = $("#formData").serializeArray();
+    book.add(formData);
+    $("#formData")[0].reset();
+    console.log('add....');
+});
+
+$(document).on('click', '.edit', function(){
+   let dataKey = $(this).attr('data');
+   $(".action_btn").attr('id', 'update');
+   $(".action_btn").html('Update Book To List');
+   $(".form_title").html('Update Book To List');
+   book.bookKey = dataKey;
+   book.edit();
+});
+
+$(document).on('click', '#update', function(e){
+    e.preventDefault();
+    let formData = $("#formData").serializeArray();
+    book.update(formData);
+    $(".action_btn").attr('id', 'add');
+    $(".action_btn").html('Add Book To List');
+    $(".form_title").html('Add Book To List');
+    $("#formData")[0].reset();
+    console.log('update....');
+});
+
+$(document).on('click', '.delete', function(){
+    let dataKey = $(this).attr('data');
+    book.delete(dataKey);
+});
